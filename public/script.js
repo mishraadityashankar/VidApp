621831dc85aad740e446e4c4e5b81d5f8f2ed312
mishraadityashankar <adityashankarmishra3@gmail.com>
2021-11-03 19:44:10 +0530

    peer connected

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
- 
+
+
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+var peer = new Peer(undefined,{
+    path: '/peerjs',
+    host: '/',
+    port: '4000'
+});
 let myVideoStream 
 navigator.mediaDevices.getUserMedia({
     video: true,
@@ -9,8 +17,36 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected',(userId) => {
+        setTimeout(() => {
+            // user joined
+            connectToNewUser(userId, stream)
+          }, 1000)
+    })
 })
 
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+
+const connectToNewUser = (userId, stream) => {
+  // console.log('new user', userId);
+  const call = peer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+  })
+}
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
 const {v4: uuidv4} = require('uuid');
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
+
+app.use('/peerjs', peerServer);
 app.get('/',(req,res)=>{
     res.redirect(`/${uuidv4()}`);
 })
@@ -16,8 +22,10 @@ app.get('/:room', (req,res)=>{
 })
 
 io.on('connection',socket => {
-    socket.on('join-room', () =>{
+    socket.on('join-room', (roomId, userId) =>{
         console.log('joined room');
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected', userId);
     })
 })
 server.listen(4000,()=>{
